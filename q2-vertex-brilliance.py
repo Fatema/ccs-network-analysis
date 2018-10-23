2224c8745693a3591a60b538e612e6aaf3a8da1d
FAlkhanaizi <fatema.alkhanaizi@durham.ac.uk>
2018-10-23 12:30:16 +0100

    added vertex_brilliance_distribution

--- q2-vertex-brilliance.py
+++ q2-vertex-brilliance.py
@@ -13,7 +13,6 @@ def vertex_brilliance(graph, source):
             for u in graph[v]:
               if u in kstar:
                   kstar[v] -= 1
-        print('kstar for ',source, ' ', kstar)
         minkstar = min(kstar.values())
         if  minkstar == 0: break
         else:
@@ -27,8 +26,27 @@ def vertex_brilliance(graph, source):
     return len(kstar)
 
 
-graph = {'a': {3,7,1}, 1: {'a',3,2}, 2: {3,1}, 3: {'a', 1,2,4,6}, 4: {3,5}, 5: {4,7,6}, 6: {3,7,5,8}, 7: {'a',6,5,8,9}, 8: {6,7}, 9: {7}}
+def vertex_brilliance_distribution(graph):
+    """Takes a graph and computes the unnormalized distribution of the degrees of the nodes.
+    Returns a dictionary whose keys correspond to degrees of nodes in the graph and values are the number of nodes
+    with that degree.
+    Degrees with no corresponding nodes in the graph are not included in the dictionary."""
+    brilliance = {}
+    for v in graph:
+        brilliance[v] = vertex_brilliance(graph, v)
 
-for v in graph:
-    brilliance = vertex_brilliance(graph, v)
-    print('b(',v,')=',brilliance)
+    # initialize dictionary for degree distribution
+    brilliance_distribution = {}
+    # consider each vertex
+    for vertex in brilliance:
+        # update degree_distribution
+        if brilliance[vertex] in brilliance_distribution:
+            brilliance_distribution[brilliance[vertex]] += 1
+        else:
+            brilliance_distribution[brilliance[vertex]] = 1
+    return brilliance_distribution
+
+
+graph = {1: {3,2}, 2: {3,1}, 3: {1,2,4,6}, 4: {3,5}, 5: {4,7,6}, 6: {3,7,5,8}, 7: {6,5,8,9}, 8: {6,7}, 9: {7}}
+
+print(vertex_brilliance_distribution(graph))
